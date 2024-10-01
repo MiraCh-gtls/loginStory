4dbf8c9cf1e024bc05cafa16d4dcc999a67def56
MiraCh-gtls <mirac@gtls.com.au>
2024-10-01 12:07:46 +0300

    add return statement

--- src/LoginClass.php
+++ src/LoginClass.php
@@ -110,25 +110,25 @@ final class LoginClass
 
                         $request->session()->save();
                         if ($request->session()->get('newRoute') && $request->session()->get('user')) {
-                            return (['user'=> $user, 'request' => $request, 'status' => 200, 'message' => 'Login successful']);
+                            return json_encode(['user'=> $user, 'request' => $request, 'status' => 200, 'message' => 'Login successful']);
                         }
                         }else{
                             $errorMessage = 'Something went wrong, try again later';
                             $statusCode = 500;
-                            return (['user'=> null, 'request' => $request, 'status' => $statusCode, 'message' => $errorMessage]);
+                            return json_encode(['user'=> null, 'request' => $request, 'status' => $statusCode, 'message' => $errorMessage]);
                         }
 
 
                 } else {
                     $errorMessage = 'Invalid Credentials';
                     $statusCode = 500;
-                    return (['user'=> null, 'request' => $request, 'status' => $statusCode, 'message' => $errorMessage]);
+                    return json_encode(['user'=> null, 'request' => $request, 'status' => $statusCode, 'message' => $errorMessage]);
                 }
             }
         } else {
             $errorMessage = 'Invalid Credentials';
             $statusCode = 500;
-            return (['user'=> null, 'request' => $request, 'status' => $statusCode, 'message' => $errorMessage]);
+            return json_encode(['user'=> null, 'request' => $request, 'status' => $statusCode, 'message' => $errorMessage]);
         }
     }
 
@@ -155,7 +155,7 @@ final class LoginClass
             $request->session()->regenerateToken();
     
             // Respond with success (Azure AD logout will be handled on the frontend)
-            return (['status' => 200, 'message' => 'Logged out locally. Handle Azure AD logout on frontend.']);
+            return json_encode(['status' => 200, 'message' => 'Logged out locally. Handle Azure AD logout on frontend.']);
         } else {
             // If user is found, proceed with API logout
             $UserId = $user->original['UserId'];
@@ -184,10 +184,10 @@ final class LoginClass
                 $request->session()->regenerateToken();
     
                 // Respond with success (Azure AD logout will be handled on the frontend)
-                return (['status' => 200, 'message' => 'Logged out locally. Handle Azure AD logout on frontend.']);
+                return json_encode(['status' => 200, 'message' => 'Logged out locally. Handle Azure AD logout on frontend.']);
             } else {
                 // Handle failure in the external API call
-                return (['status' => 500, 'message' => 'Logout failed. Please try again.']);
+                return json_encode(['status' => 500, 'message' => 'Logout failed. Please try again.']);
             }
         }
     }
@@ -235,7 +235,6 @@ final class LoginClass
                     setcookie($name, '', $expiration, '/', $sessionDomain, true);
                 }
                 $request->session()->regenerateToken();
-                // return redirect('/login');
         }} else {
                 // Invalidate and flush the session
                 $request->session()->forget('user');
