51a8b7047ee26579d7e99c4aa8f9aea67a4a3e59
MiraCh-gtls <mirac@gtls.com.au>
2024-10-02 08:28:19 +0300

    add microsoft login

--- src/LoginClass.php
+++ src/LoginClass.php
@@ -5,7 +5,8 @@ namespace gtls\loginstory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
-
+use Laravel\Socialite\Facades\Socialite;
+use Carbon\Carbon\Exception;
 
 final class LoginClass
 {
@@ -256,6 +257,118 @@ final class LoginClass
 
             // Regenerate the session token
             $request->session()->regenerateToken();
+        }
+    }
+
+    public function handleCallback(Request $request)
+    {
+        $parameters = request()->all();
+        $redirectRoute = $parameters['RedirectRoute'] ?? '/';
+        $gtamUrl = $parameters['URL'] ?? '/';
+        
+        if (session()->has('user')) {
+            return redirect()->route($redirectRoute);  // Redirect if session exists
+        }
+
+        // Proceed with the login flow if the session does not exist
+        try {
+            $socialiteUser = Socialite::driver('azure')->user();
+            $accessToken = $socialiteUser->token;
+            $expiresIn = $socialiteUser->expiresIn;
+
+            // Send request to external API for validation
+            $headers = ['Authorization' => $accessToken];
+
+            $response = Http::withHeaders($headers)->get($gtamUrl . "validate/MicrosoftToken");
+
+            if ($response->successful()) {
+                $responseJson = $response->json();
+                session()->regenerate();
+                session()->put('user', $responseJson);
+                session()->put('user_id', $responseJson['UserId']);
+                session()->put('newRoute', route('azurelogin'));
+
+                // Insert into custom_sessions
+                DB::table('custom_sessions')->insert([
+                    'id' => session()->getId(),
+                    'user_id' => $responseJson['UserId'],
+                    'payload' => session()->get('_token'),
+                    'user' => json_encode($responseJson),
+                    'last_activity' => time(),
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]);
+
+                return response()->json([
+                    'message' => 'Login successful',
+                    'access_token' => $accessToken,
+                    'expires_in' => $expiresIn,
+                    'user' => $responseJson,
+                ]);
+            }
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Authentication error: ' . $e->getMessage(),
+            ], 500);
+        }
+    }
+
+
+    public function sendToken(Request $request){
+        $parameters = request()->all();
+        $accessToken = $parameters['accessToken'];
+        $expiresIn = $parameters['expiresOn'] ?? '/';
+        $gtamUrl = $parameters['URL'] ?? '/';
+
+        // find the user in the database through API
+        $url = $gtamUrl . "validate/MicrosoftToken";
+
+        $headers = [
+            'Authorization' => $accessToken,
+        ];
+
+        // Send the logout request to the external API
+        $response = Http::withHeaders($headers)->post($url);
+
+        if ($response->successful()) {
+            // $responseBody = $response->body();
+            $responseJson = $response->json();
+
+            $jsonString = json_encode($responseJson);
+
+            $request->session()->regenerate();
+            $request->session()->put('user', json_encode($responseJson[0]));
+            $request->session()->put('user_id', $responseJson[0]['UserId']);
+            $request->session()->put('newRoute', route('azure.login'));
+
+            $sessionId = $request->session()->getId();
+            $payload = $request->session()->get('_token');
+            $userSession = $request->session()->get('user');
+            $user = $jsonString;
+            $lastActivity = time();
+
+            DB::table('custom_sessions')->insert([
+                'id' => $sessionId,
+                'user_id' => $responseJson[0]['UserId'],
+                'payload' => $payload,
+                'user' => $userSession,
+                'last_activity' => $lastActivity,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+            $request->session()->save();
+
+            return response()->json([
+                'message' => 'Login successful',
+                'access_token' => $accessToken,
+                'expires_in' => $expiresIn,
+                'user' => $user,
+            ]);
+        } else {
+            return response()->json([
+                'message' => 'Authentication error: ' . $response->body(),
+            ], 500);
+
         }
     }
 }
